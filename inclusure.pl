d742891374c92f3b7a1f8b9efb31c6aab773affb
Peter Vereshagin <peter@vereshagin.org>
2011-06-25 11:01:16 +0400

    comments

--- inclusure.pl
+++ inclusure.pl
@@ -6,8 +6,12 @@ use warnings;
 use Config;
 use File::Find;
 
+# Hash in the form directory => file_extension to search for dupes for (not
+# from)
 my %dirext = ( $Config{ 'privlib' } => 'pm', $Config{ 'archlib' } => 'so' );
 my @dirs = keys %dirext;
+
+# Directories where to search dupes
 my @incs;
 foreach my $noodle (@INC) {
     unless ( grep { $_ eq $noodle } @dirs ) {
@@ -16,20 +20,28 @@ foreach my $noodle (@INC) {
 }
 
 while ( my ( $dir => $ext ) = each %dirext ) {
+
+    # For every directory make common regex and length for substr
     my $regex   = qr/\.$ext$/;
     my $dir_len = length $dir;
     find(
-        {   no_chdir => 1,
-            wanted   => sub {
+        {   no_chdir => 1,      # exclude '.' which is typically found in @INC
+            wanted   => sub {   # executed on every file in a dir
                 my $fn = $File::Find::name;
                 if ( $fn =~ m/$regex/ ) {
+
+                    # Short name is a part of a filer name to search in
+                    # another directories
                     my $sn = substr( $fn, $dir_len, length($fn) - $dir_len );
                     foreach my $pigdir (@incs) {
+
+                        # Pig name is a name of a file which is a dupe if
+                        # exists
                         my $pigname = "$pigdir$sn";
                         if ( -f $pigname ) { print "$pigname\n" }
                     }
                 }
                 }
-        } => $dir
+        } => $dir    # directory is another argument for find()
     );
 }
